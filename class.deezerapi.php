fa8ae7ad4ead16bd6b45d80a524b25cfa3a1d913
croustibat <bbouillot@gmail.com>
2012-06-14 16:32:43 +0200

    start code...

--- class.deezerapi.php
+++ class.deezerapi.php
@@ -1,9 +1,37 @@
 <?php
 
 
-class deezerapi{
+class deezerapi {
 
+	public $config = array(
+			'app_id' 		=> "YOUR_APP_ID",
+			'app_secret' 	=> "YOUR_APP_SECRET",
+			'my_url'     	=> "YOUR_CALLBACK_URL"
+		);
 	
+	public function __construct( $config = array() ){
+
+		// surcharge config si present
+		if (!empty($config)) {
+			$this->config = $config;
+		}
+
+	}
+
+
+	public function getTrack(){}
+
+	public function getPlaylist(){}
+
+	public function getAlbum(){}
+
+	public function getFolder(){}
+
+	private final function call_method($method, $params = array()){
+
+
+	}
+
 }
 
 ?>
