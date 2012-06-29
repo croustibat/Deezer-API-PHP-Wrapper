cdd3343db81dfe8cdc4cc484f8f27773c675f7ba
croustibat <bbouillot@gmail.com>
2012-06-29 12:04:20 +0200

    add example in search sample

--- samples/search.php
+++ samples/search.php
@@ -3,17 +3,23 @@ require '../class.deezerapi.php';
 
 $dz = new deezerapi();
 
-/* SEARCH AN ARTIST */
+/* SEARCH TRACKS */
 $term = 'weezer'; // term to search on
-$context = ''; // by default search by artist
+$context = ''; // by default search title
 $orderby = 'RANKING'; 
 
 $searchDataArtist = $dz->search($term, $context, $orderby);
  
 
+/* SEARCH ARTIST */
+$term = 'weezer'; // term to search on
+$context = 'artist'; // by default search title
+$orderby = 'RANKING'; 
 
+$searchDataArtist = $dz->search($term, $context, $orderby);
+ 
 
-/* SEARCH AN ALBUM */
+/* SEARCH ALBUM */
 $term = 'war on errorism'; // term to search on
 $context = 'album';
 $orderby = 'RANKING'; 
